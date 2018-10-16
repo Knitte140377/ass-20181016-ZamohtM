6a15ced6b9dca06b6bfb1339efd49884c26c281a
Thomas <43405974+ZamohtM@users.noreply.github.com>
2018-10-16 16:28:07 +0200

    Update reeeeee.c

--- reeeeee.c
+++ reeeeee.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 int SquareArea(int a) {
     
@@ -25,44 +26,47 @@ int RectTriangleArea(int a, int b) {
     return (a*b)/2;
 }
 
-int RectTriangleCirc(int a, int b) {
+double RectTriangleCirc(int a, int b) {
     
-    return 
+    double c = sqrt(pow(a,2)+pow(b,2));
+    return a+b+c;
 }
 
-int CircleArea(int r) {
+double CircleArea(double r) {
     
-    return 
+    return pow(r,2)*3.14;
 }
 
-int CircleCir(int r) {
+double CircleCir(double r) {
     
-    return 
+    return 2*r*3.14;
 }
 
-int RingArea(int r1, int r2) {
+double RingArea(double r1, double r2) {
     
-    return 
+    return 3.14*(pow(r1,2)-pow(r2,2));
 }
 
-int RingCir(int r1, int r2) {
+double RingCir(double r1, double r2) {
     
-    return 
+    double u1 = 2*r1*3.14;
+    double u2 = 2*r2*3.14;
+    return u1 + u2;
 }
 
-int Current(int U,U) {
+double Current(double U,double R) {
     
-    return 
+    return U/R;
 }
 
-int Ps2KW(int ps) {
+double Ps2KW(double ps) {
     
-    return 
+    return ps/1.36;
 }
 
-int Kw2PS(int kw) {
+double Kw2PS(double kw) {
     
-    return 
+    return kw*1.36;
 }
 
 
@@ -70,20 +74,27 @@ int main()
 {
     int a = 2;
     int b = 2;
+    double r = 2;
+    double r1 = 2;
+    double r2 = 4;
+    double R = 2;
+    double U = 2;
+    double ps = 2;
+    double kw = 2;
     
     printf("Result: %d\n",SquareArea(a));
     printf("Result: %d\n",SquareCirc(a));
     printf("Result: %d\n",RectArea(a,b));
     printf("Result: %d\n",RectCirc(a,b));
     printf("Result: %d\n",RectTriangleArea(a,b));
-    printf("Result: %d\n",RectTriangleCirc(a,b));
-    printf("Result: %d\n",CircleArea(r));
-    printf("Result: %d\n",CircleCir(r));
-    printf("Result: %d\n",RingArea(r1,r2));
-    printf("Result: %d\n",RingCir(r1,r2));
-    printf("Result: %d\n",Current(U,U));
-    printf("Result: %d\n",Ps2KW(ps));
-    printf("Result: %d\n",Kw2PS(kw));
-    
+    printf("Result: %f\n",RectTriangleCirc(a,b));
+    printf("Result: %f\n",CircleArea(r));
+    printf("Result: %f\n",CircleCir(r));
+    printf("Result: %f\n",RingArea(r1,r2));
+    printf("Result: %f\n",RingCir(r1,r2));
+    printf("Result: %f\n",Current(U,R));
+    printf("Result: %f\n",Ps2KW(ps));
+    printf("Result: %f\n",Kw2PS(kw));
+
     return 0;
 }
