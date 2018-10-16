4de6cf04a37db5124366e148c3a883e8fefa383e
Thomas <43405974+ZamohtM@users.noreply.github.com>
2018-10-16 14:04:57 +0200

    Create reeeeee.c

---
+++ reeeeee.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+
+int SquareArea(int a) {
+    
+    return a*a;
+}
+
+int SquareCirc(int a) {
+    
+    return a*4;
+}
+
+int RectArea(int a, int b) {
+    
+    return a*b;
+}
+
+int RectCirc(int a, int b) {
+    
+    return 2*(a+b);
+}
+
+int RectTriangleArea(int a, int b) {
+    
+    return (a*b)/2;
+}
+
+int RectTriangleCirc(int a, int b) {
+    
+    return 
+}
+
+int CircleArea(int r) {
+    
+    return 
+}
+
+int CircleCir(int r) {
+    
+    return 
+}
+
+int RingArea(int r1, int r2) {
+    
+    return 
+}
+
+int RingCir(int r1, int r2) {
+    
+    return 
+}
+
+int Current(int U,U) {
+    
+    return 
+}
+
+int Ps2KW(int ps) {
+    
+    return 
+}
+
+int Kw2PS(int kw) {
+    
+    return 
+}
+
+
+int main()
+{
+    int a = 2;
+    int b = 2;
+    
+    printf("Result: %d\n",SquareArea(a));
+    printf("Result: %d\n",SquareCirc(a));
+    printf("Result: %d\n",RectArea(a,b));
+    printf("Result: %d\n",RectCirc(a,b));
+    printf("Result: %d\n",RectTriangleArea(a,b));
+    printf("Result: %d\n",RectTriangleCirc(a,b));
+    printf("Result: %d\n",CircleArea(r));
+    printf("Result: %d\n",CircleCir(r));
+    printf("Result: %d\n",RingArea(r1,r2));
+    printf("Result: %d\n",RingCir(r1,r2));
+    printf("Result: %d\n",Current(U,U));
+    printf("Result: %d\n",Ps2KW(ps));
+    printf("Result: %d\n",Kw2PS(kw));
+    
+    return 0;
+}
